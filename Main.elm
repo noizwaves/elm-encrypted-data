8e992864ed5a3e9caa8790d4d0ecadf33fe0cf13
Adam Neumann <noizwaves@users.noreply.github.com>
2018-04-01 15:17:40 -0600

    Added decrypt functionality

--- Main.elm
+++ Main.elm
@@ -4,7 +4,7 @@ import Html.Attributes exposing (value, rows, cols)
 
 import Random exposing (Seed, initialSeed)
 --import Crypto.Types exposing (Passphrase, Plaintext, Ciphertext)
-import Crypto.Strings exposing (encrypt)
+import Crypto.Strings exposing (encrypt, decrypt)
 
 type alias Model =
     { message: String
@@ -16,7 +16,9 @@ type alias Model =
 type Msg
     = ChangeMessage String
     | ChangeKey String
+    | ChangeEncrypted String
     | DoEncryption
+    | DoDecryption
 
 update : Msg -> Model -> Model
 update msg model =
@@ -25,6 +27,8 @@ update msg model =
             { model | message = message }
         ChangeKey key ->
             { model | key = key }
+        ChangeEncrypted encrypted ->
+            { model | encrypted = encrypted }
         DoEncryption ->
             let
                 result = encrypt model.seed model.key model.message
@@ -33,7 +37,16 @@ update msg model =
                     Ok (cypherText, seed) ->
                         { model | encrypted = cypherText, seed = seed }
                     Err errorMessage ->
-                        { model | encrypted = errorMessage }
+                        model
+        DoDecryption ->
+            let
+                result = decrypt model.key model.encrypted
+            in
+                case result of
+                    Ok plainText ->
+                        { model | message = plainText }
+                    Err errorMessage ->
+                        model
 
 view : Model -> Html Msg
 view model = div []
@@ -47,9 +60,10 @@ view model = div []
         , input [ onInput ChangeKey, value model.key ] []
         ]
     , button [ onClick DoEncryption ] [ text "Encrypt" ]
+    , button [ onClick DoDecryption ] [ text "Decrypt" ]
     , hr [] []
     , label [] [ text "Encrypted Data: " ], br [] []
-    , textarea [ value model.encrypted, rows 8, cols 60 ] [ ]
+    , textarea [ value model.encrypted, onInput ChangeEncrypted, rows 8, cols 60 ] [ ]
     ]
 
 main = Html.beginnerProgram
