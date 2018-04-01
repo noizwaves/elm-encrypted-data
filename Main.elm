ea23591d7d670ece50882047c7d4dc1920c89009
Adam Neumann <noizwaves@users.noreply.github.com>
2018-04-01 15:35:20 -0600

    Use load time as random seed

--- Main.elm
+++ Main.elm
@@ -2,9 +2,12 @@ import Html exposing (Html, br, button, div, hr, input, label, text, textarea)
 import Html.Events exposing (onInput, onClick)
 import Html.Attributes exposing (value, rows, cols)
 
+import Task
+import Time exposing (Time, inMilliseconds)
+
 import Random exposing (Seed, initialSeed)
---import Crypto.Types exposing (Passphrase, Plaintext, Ciphertext)
 import Crypto.Strings exposing (encrypt, decrypt)
+
 
 type alias Model =
     { message: String
@@ -13,40 +16,49 @@ type alias Model =
     , seed: Seed
     }
 
+
 type Msg
     = ChangeMessage String
     | ChangeKey String
     | ChangeEncrypted String
+    | TimeResult Time
     | DoEncryption
     | DoDecryption
 
-update : Msg -> Model -> Model
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         ChangeMessage message ->
-            { model | message = message }
+            { model | message = message } ! []
         ChangeKey key ->
-            { model | key = key }
+            { model | key = key } ! []
         ChangeEncrypted encrypted ->
-            { model | encrypted = encrypted }
+            { model | encrypted = encrypted } ! []
         DoEncryption ->
             let
                 result = encrypt model.seed model.key model.message
             in
                 case result of
                     Ok (cypherText, seed) ->
-                        { model | encrypted = cypherText, seed = seed }
+                        { model | encrypted = cypherText, seed = seed } ! []
                     Err errorMessage ->
-                        model
+                        model ! []
         DoDecryption ->
             let
                 result = decrypt model.key model.encrypted
             in
                 case result of
                     Ok plainText ->
-                        { model | message = plainText }
+                        { model | message = plainText } ! []
                     Err errorMessage ->
-                        model
+                        model ! []
+        TimeResult time ->
+            let
+                ms = floor (inMilliseconds time)
+            in
+                { model | seed = initialSeed ms } ! []
+
 
 view : Model -> Html Msg
 view model = div []
@@ -66,8 +78,23 @@ view model = div []
     , textarea [ value model.encrypted, onInput ChangeEncrypted, rows 8, cols 60 ] [ ]
     ]
 
-main = Html.beginnerProgram
-    { model = Model "{\"Hello\":\"World\"}" "s0m3p455phr453" ""  (initialSeed 0)
+
+init : (Model, Cmd Msg)
+init =
+    let
+        model =
+            { message = "{\"Hello\":\"World\"}"
+            , key = "s0m3p455phr453"
+            , encrypted = ""
+            , seed = initialSeed 0
+            }
+     in
+        model ! [ Task.perform TimeResult Time.now ]
+
+
+main = Html.program
+    { init = init
     , view = view
     , update = update
+    , subscriptions = \m -> Sub.none
     }
